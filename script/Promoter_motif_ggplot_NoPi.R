67bb5fe88c3d924291f9f33b96f405bf6c9e3bde
Jia-Zhao1998 <67213486+Jia-Zhao1998@users.noreply.github.com>
2021-05-07 21:35:08 -0500

    Add motif
    No Pi: Promoter position vs Nucleosome occupancy + Motifs

---
+++ script/Promoter_motif_ggplot_NoPi.R
@@ -0,0 +1,18 @@
+# This function is to plot the nucleosome occupancy score aginst the promoter region of a gene and also to plot
+# the Pho4 consensus motif and 1-bp off motif of Pho4 along with the genomic position. 
+
+Promoter_motif_ggplot_NoPi <- function(Annotation_data = S288cAnnotation_sacCer1, motif = sacCer1Motif, motif_rev = sacCer1REVMotif, gene = "YBR093C"){
+  line <- Annotation_data[which(Annotation_data$gene == gene),]
+  if (line$strand == "-"){
+    motifs <- motif[which(motif$chromosome == line$chromosome & as.numeric(motif$start) 
+                          >= line$end & as.numeric(motif$end) <= line$end + 800),]
+    Promoter_ggplot_NoPi(data = Annotation_data, gene = gene) + geom_segment(aes(x = as.numeric(start), y = 0, xend = as.numeric(end), yend = 0), 
+                                        data = motifs, size = 5, col = ifelse(motifs$motif=="CACGTG", "red","blue"))
+  }else{
+    motifs <- motif[which(motif$chromosome == line$chromosome & as.numeric(motif$start)
+                               >= line$start - 800 & as.numeric(motif$end) <= line$start),]
+    Promoter_ggplot_NoPi(data = Annotation_data, gene = gene) + geom_segment(aes(x = as.numeric(start), y = 0, xend = as.numeric(end), yend = 0), 
+                                     data = motifs_rev, size = 5, col = ifelse(motifs$motif=="GTGCAC", "hotpink","cyan"))
+  }()
+}
+
